9aa51a99c26111bb34b71775d2b79ffa1e9e1c9e
shedrack32 <shedrackgabriel98@gmail.com>
2025-08-09 13:49:55 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,1 +1,68 @@
-
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'zokk',
+    PREFIXE: process.env.PREFIX || "+",
+    GITHUB : process.env.GITHUB|| 'https://github.com/ZEZE47-MD/ZEZE47',
+    OWNER_NAME : process.env.OWNER_NAME || "𝐙𝐄𝐙𝐄𝟒𝟕-𝐌𝐃",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255682937675",  
+    ANTI_LINK : process.env.ANTI_LINK || "yes",
+    ANTI_BAD : process.env.ANTI_BAD || "yes",               
+    AUTO_REPLY : process.env.AUTO_REPLY || "yes",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTO_REACT: process.env.AUTO_REACTION || "non",  
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'non',              
+    CHATBOT: process.env.CHAT_BOT || "off",              
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "yes",
+    AUTO_BLOCK: process.env.BLOCK_ALL || 'yes',              
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029VbANIT5D8SDpK7oExi1v",
+    WEBSITE :process.env.GURL || "https://whatsapp.com/channel/0029VbANIT5D8SDpK7oExi1v",
+    CAPTION : process.env.CAPTION || "𝘡𝘌𝘡𝘌47 𝘛𝘌𝘊𝘏",
+    BOT : process.env.BOT_NAME || '𝐙𝐄𝐙𝐄𝟒𝟕-𝐌𝐃',
+    URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/q4lv9f.jpg',
+    MODE: process.env.PUBLIC_MODE || "no",              
+    TIMEZONE: process.env.TIMEZONE || "Africa/Nairobi", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+                  URL : process.env.URL || 'https://files.catbox.moe/avz1u3.jpg',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_API_KEY : process.env.HEROKU_API_KEY || null,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ETAT : process.env.PRESENCE || '',
+    GEMINI_API_KEY : process.env.GEMINI_API_KEY || 'AIzaSyCcZqDMBa8FcAdBxqE1o6YYvzlygmpBx14',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'yes',
+    ANTICALL: process.env.ANTICALL || 'yes',              
+    CHATBOT : process.env.CHATBOT || 'yes',  
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
